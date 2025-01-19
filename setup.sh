22fcc20f03e354ee05ba71ed6fd293d8cd97dce8
piSky <davidsomerville@comcast.net>
2025-01-18 19:43:49 -0500

    Updated setup based on requirements

--- setup.sh
+++ setup.sh
@@ -32,6 +32,40 @@ pip install skyfield --break-system-packages
 
 pip install pandas --break-system-packages
 
+export PATH=$PATH:/home/pi/.local/bin
+
+# Install skyfield https://rhodesmill.org/skyfield/
+echo 4. Install i2c and IO support
+
+
+
+echo 3. Installing OPi.GPIO 
+# Install GPIO support for the orange PI 
+# see https://pypi.org/project/RPi.GPIO/ and https://sourceforge.net/p/raspberry-gpio-python/wiki/Home/ 
+# Note: Use GPIO.setmode(GPIO.SUNXI) to use "PA01" style channel naming
+pip install RPi.GPIO --break-system-packages
+# Enable i2c hardware
+sudo raspi-config nonint do_i2c 0
+# Enable serial hardware but not console thru serial
+sudo raspi-config nonint do_serial 2
+
+echo 4. Installing python i2c and oled support
+# Adafruit version (Circuit python and python)
+# https://github.com/adafruit/Adafruit_CircuitPython_SSD1306/tree/main 
+# Use i2cdetect to make sure you see the i2c device on the I2C 1 bus (Pins 3 and 5)  
+# i2cdetect -y 1
+echo 4a. Install i2c utilities
+#  Can run i2c scans i.e. 'i2cdetect -y 1'
+sudo apt-get install -y i2c-tools
+# Give pi user access to i2c
+sudo usermod -a -G spi,gpio,i2c pi
+echo 4b. OLED Installing adafruit i2c and oled support
+pip3 install adafruit-circuitpython-ssd1306 --break-system-packages
+pip3 install adafruit-circuitpython-pca9685 --break-system-packages
+# GPS module connect via serial
+# see https://sparklers-the-makers.github.io/blog/robotics/use-neo-6m-module-with-raspberry-pi/
+pip install pynmea2 --break-system-packages
+# Note: compase uses gy271compass.py library
 
 # # Add iotLoader.service to the /lib/systemd/system/ folder
 # # By default service is not enabled 
@@ -41,11 +75,11 @@ pip install pandas --break-system-packages
 # # sudo systemctl start iotLoader.service 
 # sudo systemctl status iotLoader.service -n50
 
-echo Install fastapi for web services and a ASGI web server
-pip install fastapi --break-system-package
-pip install "uvicorn[standard]" --break-system-package
-pip install python-multipart --break-system-package
-export PATH=$PATH:$HOME/.local/bin
+# echo Install fastapi for web services and a ASGI web server
+# pip install fastapi --break-system-package
+# pip install "uvicorn[standard]" --break-system-package
+# pip install python-multipart --break-system-package
+# export PATH=$PATH:$HOME/.local/bin
 
 # # Add iotWS.service to the /lib/systemd/system/ folder
 # # By default service is not enabled 
