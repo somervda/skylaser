54d52f9aca28264f4ab459609ad4034809b07280
piSky <davidsomerville@comcast.net>
2025-01-31 15:24:18 -0500

    Improved servo tester

--- servoTester.py
+++ servoTester.py
@@ -1,13 +1,32 @@
 from adafruit_servokit import ServoKit
 import time
 kit = ServoKit(channels=16)
-kit.servo[15].set_pulse_width_range(650, 2600)
-angle = 0
+kit.servo[1].set_pulse_width_range(700, 2400)
+rangle = 0
+kit.servo[0].angle = 0
+time.sleep(1)
+kit.servo[1].angle = 0
+print(0,0)
+time.sleep(10)
+kit.servo[0].angle = 180
+time.sleep(1)
+kit.servo[1].angle = 90
+print(180,180)
+time.sleep(10)
 while True:
-    angle +=5
-    if angle>180:
-        angle=0
-    kit.servo[15].angle = angle
-    print(angle)
-    time.sleep(.3)
+    if rangle>180:
+        rangle=0
+    dangle = 0
+    while True:
+        if dangle>90:
+            dangle=0
+            break
+        print(rangle,dangle)
+        kit.servo[0].angle = rangle
+        kit.servo[1].angle = dangle
+        if dangle==0:
+            time.sleep(1)
+        dangle +=5
+        time.sleep(.1)
+    rangle +=5
 
