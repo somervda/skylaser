713d5a98ba24da43d50c3a6340bd3ffcef8e75ab
piSky <davidsomerville@comcast.net>
2025-03-01 07:45:46 -0500

    Improved gimbal tester, go though a standard set of positions

--- gmTester.py
+++ gmTester.py
@@ -1,3 +1,6 @@
 from gimbalManager import GimbalManager
 gm=GimbalManager()
-gm.move(0,45)
+for azimuth in range(0,360,45):
+    for altitude in range(0,100,45):
+        gm.move(azimuth,altitude)
+gm.move(0,90)
